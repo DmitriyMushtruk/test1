401bf1833bfcdef1b08e915088cc800859138f00
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2022-10-06 23:31:10 +0200

    Another commit.

--- src/pl/vistulaMushtruk/HelloWorldMushtruk.java
+++ src/pl/vistulaMushtruk/HelloWorldMushtruk.java
@@ -3,5 +3,9 @@ package pl.vistulaMushtruk;
 public class HelloWorldMushtruk {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Mushtruk");
+        System.out.println("This is test!");
+        System.out.println("Hello World!");
+        System.out.println("See you on GitHub!");
+        System.out.println("Bye!");
     }
 }
